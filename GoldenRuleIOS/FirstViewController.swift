aa0b852f40ea975f02137e46f1e322faa64ed93b
user909394 <user909394@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2017-12-27 11:23:31 -0800

    Initial Commit

---
+++ GoldenRuleIOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GoldenRuleIOS
+//
+//  Created by user909394 on 12/27/17.
+//  Copyright © 2017 JohnTuttle. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
