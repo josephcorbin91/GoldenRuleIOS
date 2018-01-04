7e5dc56db747acd275bba5bcc4e56ba6a1c3b84b
josephcorbin91 <joseph.corbin91@yahoo.com>
2018-01-04 15:08:30 -0500

    Update Organization.swift

--- GoldenRuleIOS/Organization.swift
+++ GoldenRuleIOS/Organization.swift
@@ -1,1 +1,32 @@
 
+//
+//  Organization.swift
+//  
+//
+//
+import Foundation
+
+struct Organziation {
+	let organization_id: String?
+	let message: URL?
+	let date: Int?
+  let createDate: Int?
+}
+
+extension Organization {
+	private enum Keys: String, SerializationKey {
+		case reputation
+		case name = "display_name"
+		case profileImage = "profile_image"
+	}
+	
+	init(serialization: Serialization) {
+		name = serialization.value(forKey: Keys.name)
+		reputation = serialization.value(forKey: Keys.reputation)
+		if let url: String = serialization.value(forKey: Keys.profileImage) {
+			profileImageURL = URL(string: url)
+		} else {
+			profileImageURL = nil
+		}
+	}
+}
