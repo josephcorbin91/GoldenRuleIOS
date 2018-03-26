d54d6e38723a9be2b70884337e68145c352e5406
josephcorbin91 <joseph.corbin91@yahoo.com>
2018-03-25 19:18:28 -0700

    Update HomeViewController.swift

--- GoldenRuleIOS/HomeViewController.swift
+++ GoldenRuleIOS/HomeViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+include UserNotifications
 
+var notificationGranted=false
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        UNUserNotificationCenter.current().requestAuthorization(
+        options: [.alert,.sound]) 
+        { 
+            (granted, error) in
+            self.notificationGranted = granted
+            if let error = error {
+                print("granted, but Error in notification permission:\(error.localizedDescription)")
+        }
+            
         UIGraphicsBeginImageContext(self.view.frame.size)
         UIImage(named: "home")?.draw(in: self.view.bounds)
         
@@ -24,9 +36,37 @@ class HomeViewController: UIViewController {
             
         }
 
+            if notificationGranted{
+            repeatNotification()
+            }  
         // Do any additional setup after loading the view.
     }
 
+         func repeatNotification(){
+             
+             var dateComponents = DateComponents()
+// a more realistic example for Gregorian calendar. Every Monday at 11:30AM
+//dateComponents.hour = 11
+//dateComponents.minute = 30
+dateComponents.second = 0
+             let trigger = UNCalendarNotificationTrigger(
+    dateMatching: dateComponents, 
+    repeats: true)
+let content = UNMutableNotificationContent()
+content.title = "Pizza Time!!"
+content.body = "Monday is Pizza Day"
+content.categoryIdentifier = "pizza.reminder.category"
+
+let request = UNNotificationRequest(identifier: "pizza.reminder", content: content, trigger: trigger)
+ 
+UNUserNotificationCenter.current().add(request) { (error) in
+    if let error = error {
+        print("error in pizza reminder: \(error.localizedDescription)")
+    }
+}
+print("added notification:\(request.identifier)")
+
+            }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
