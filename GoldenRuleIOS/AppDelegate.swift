29f22b0b3b49a3be7368276587c759e72598a461
user132771 <user132771@74-80-239-248.ic2net.net>
2018-03-26 13:56:48 -0700

    CoreData added

--- GoldenRuleIOS/AppDelegate.swift
+++ GoldenRuleIOS/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -59,7 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "sample")
+        let container = NSPersistentContainer(name: "Model")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
--- GoldenRuleIOS/NewsViewController.swift
+++ GoldenRuleIOS/NewsViewController.swift
@@ -7,8 +7,39 @@
 //
 
 import UIKit
+import CoreData
 
 class NewsViewController: UIViewController {
+    
+    
+    func seedIOSDatabase(){
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "News", in: context)
+        let news = NSManagedObject(entity: entity!,insertInto: context)
+        news.setValue("time", forKey: "mNewsTitle")
+        news.setValue("time", forKey: "mNewsBody")
+        news.setValue("time", forKey: "mTime")
+        do {
+            try context.save()
+        }catch{
+            print("Failed saving")
+        }
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "News")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject]{
+                print(data.value(forKey: "mTime") as! String)
+            }
+        }
+        catch{
+            print("Failed")
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +53,9 @@ class NewsViewController: UIViewController {
             UIGraphicsEndImageContext()
             debugPrint("Image not available")
             
-        }        // Do any additional setup after loading the view.
+        }
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
